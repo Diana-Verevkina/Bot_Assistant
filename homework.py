14f07a69808293a53510017c403caa274219bbdf
Diana-Verevkina <diva2208@mail.ru>
2022-09-13 14:26:31 +0300

    first commit

--- homework.py
+++ homework.py
@@ -86,10 +86,7 @@ def check_response(response):
 
 
 def parse_status(homework):
-    """
-    Извлекает из информации о конкретной домашней работе
-    статус этой работы.
-    """
+    """Извлекает статус домашней работы."""
     homework_name = homework['homework_name']
     homework_status = homework['status']
 
